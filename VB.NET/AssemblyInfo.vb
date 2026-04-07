3ef97dc060a5720d4ef8ec8c52114f7ca8e49345
sl2365 <76538604+sl2365@users.noreply.github.com>
2026-04-07 14:52:11 +0100

    Add files via upload

--- VB.NET/AssemblyInfo.vb
+++ VB.NET/AssemblyInfo.vb
@@ -9,5 +9,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTrademark("")>
 <Assembly: ComVisible(False)>
 <Assembly: Guid("B2C3D4E5-F6A7-8901-BCDE-F12345678901")>
-<Assembly: AssemblyVersion("3.0.0.0")>
-<Assembly: AssemblyFileVersion("3.0.0.0")>
+<Assembly: AssemblyVersion("3.0.1.0")>
+<Assembly: AssemblyFileVersion("3.0.1.0")>
